cc24d2b65a4877057cbe33f51f85d8be4ae71d5d
Joel-Meneses <joelwork7744@gmail.com>
2025-04-01 13:07:26 -0300

    correção e organizaçao
    menu separado das outras funções e correção de linhas

--- caixa.py
+++ caixa.py
@@ -1,16 +1,13 @@
 saldod= [250]
-usuario=str(input("Ola,digite seu nome:"))
-senha=(input('digite sua senha'))
 sacado=[0]
 depositado=[0]
 
-
 def deposit(saldod, depositado):
-    depositar= input('Valor de deposito:')
+    depositar= input('valor de deposito:')
     if not depositar.isnumeric():
         print("Por favor, digite apenas números!")
     elif float(depositar) <= 0:
-        print('Sorry,insira um valor positivo para depositar ')
+        print('sorry,insira um valor positivo para depositar ')
 
     elif depositar.isnumeric():
         depositado[0] = float(depositar)
@@ -20,52 +17,23 @@ def deposit(saldod, depositado):
     
 
 def saque(saldod,sacado):
-    sacar= input('Valor do saque:')
+    sacar= input('valor do saque:')
     if not sacar.isnumeric():
         print("Por favor, digite apenas números!")
     elif float(sacar)<= 0:
-        print('Porfavor insira um valor positivo para saque')
+        print('porfavor insira um valor positivo para saque')
     elif float(sacar) > saldod[0]:
-        print('Isso e maior que seu saldo, nao foi possivel sacar,pobre')
+        print('isso e maior que seu saldo, nao foi possivel sacar,pobre')
     elif sacar.isnumeric():
         sacado[0] = float(sacar)
         saldod[0]-= sacado[0]
     else:
         print('algo deu errado, o problema e voce')
     
-    
+
 def consulta(saldod):
-    print(' Seu saldo e {} na conta corrente'.format(saldod[0]))
+    print(' seu saldo é :{} na conta corrente'.format(saldod[0]))
+
 
 def extrato(sacar,depositar):
-    print('Seu extrato...\nvalor sacado{}\n valor depositado{}'.format(sacado,depositado))
-
-
-def menu():
-    print(' Seja bem vindo {} qual operação deseja realizar?'.format(usuario))
-    global saldod
-    r = int(input('Digite 1 para consultar seu saldo;\ndigite 2 para depositar;\ndigite 3 para sacar;\n digite 4 para ver extrato\ninsira:'))
-    if r==1:
-        consulta(saldod)
-        
-
-    elif r==2:
-        deposit(saldod,depositado)
-       
-
-
-    elif r==3:
-        saque(saldod,sacado)
-
-    elif r==4:
-        extrato(sacado,depositado)
-        
-    
-    else:
-        print('Opção inexistente, escolha as opcoes')
-
-    menu()
-
-menu()
-
-
+    print('seu extrato...\nvalor sacado: {}\n valor depositado: {}'.format(sacado[0],depositado[0]))
